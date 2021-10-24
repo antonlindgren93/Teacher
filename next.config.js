f222bb4190243551d136761064f1089c7ad31b4a
Cyrille <ngidekollecyrille@gmail.com>
2021-10-24 21:44:12 +0200

    Add Category banner, create Teacher card, Fix config for webpack/babel/next

--- next.config.js
+++ next.config.js
@@ -1,12 +1,15 @@
 // next.config.js
+
 const path = require("path");
 const withPWA = require("next-pwa");
 const runtimeCaching = require("next-pwa/cache");
 
+const prod = process.env.NODE_ENV === "production";
 module.exports = withPWA({
   pwa: {
     dest: "public",
     runtimeCaching,
+    disable: prod ? false : true,
   },
 
   // This is not required to make it into a PWA, but is a nice way to clean up your imports
